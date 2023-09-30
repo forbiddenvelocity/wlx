86e1d339dd77b0efef6d19d7d8548cbfbf0358d8
Mubashir <mubashir1728@gmail.com>
2023-09-30 17:33:12 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000
 
+app.use('/', (req, res) => {
+    res.send("Hello World");
+})
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 })
