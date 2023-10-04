31393a772cc21c45103f155c76e1ecfd0460cd13
Mubashir <mubashir1728@gmail.com>
2023-10-04 21:47:42 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const dBConnect = require('./config/dbConnect');
+const bodyParser = require('body-parser');
 const app = express()
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000
 dBConnect();
-
+app.use(bodyParser.json())
 app.use('/', (req, res) => {
     res.send("Hello World");
 })
