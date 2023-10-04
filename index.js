52766998757cb8b54b4670a12bcaac526fbbaa3c
Mubashir <mubashir1728@gmail.com>
2023-10-05 00:49:42 +0530

    commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const dBConnect = require('./config/dbConnect');
+const authRouter = require('./routers/authRoute')
 const bodyParser = require('body-parser');
 const app = express()
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000
 dBConnect();
 app.use(bodyParser.json())
-app.use('/', (req, res) => {
-    res.send("Hello World");
-})
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use("/api/user", authRouter)
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 })
